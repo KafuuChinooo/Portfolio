baf6298ed3f9b9a255a0137a8c698301661020d2
littlekafuu <kafuuchinovn@gmail.com>
2026-04-08 14:57:42 +0700

    update

--- script.js
+++ script.js
@@ -70,6 +70,8 @@ const portfolioContent = {
     links: [
       { label: "GitHub", url: "https://github.com/KafuuChinooo/" },
       { label: "LinkedIn", url: "https://www.linkedin.com/in/phanconganh/" },
+      { label: "Facebook", url: "https://www.facebook.com/shuvikafuu/" },
+      { label: "Instagram", url: "https://www.instagram.com/shuvikafuu/" },
       { label: "Email", url: "mailto:phananh2492005@gmail.com" }
     ]
   },
@@ -90,6 +92,34 @@ const setHtml = (id, value) => {
   if (element) {
     element.innerHTML = value;
   }
+};
+
+const socialIcons = {
+  GitHub: `
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path fill="currentColor" d="M12 .5a12 12 0 0 0-3.79 23.39c.6.11.82-.26.82-.58v-2.05c-3.34.73-4.04-1.42-4.04-1.42-.55-1.37-1.33-1.74-1.33-1.74-1.09-.74.08-.73.08-.73 1.2.09 1.83 1.22 1.83 1.22 1.08 1.82 2.82 1.29 3.5.99.11-.76.42-1.29.76-1.58-2.66-.3-5.47-1.31-5.47-5.86 0-1.3.47-2.36 1.22-3.2-.12-.3-.53-1.52.12-3.16 0 0 1-.31 3.3 1.22a11.6 11.6 0 0 1 6 0c2.3-1.53 3.3-1.22 3.3-1.22.65 1.64.24 2.86.12 3.16.76.84 1.22 1.9 1.22 3.2 0 4.56-2.82 5.55-5.5 5.85.43.37.82 1.09.82 2.21v3.28c0 .32.22.69.83.57A12 12 0 0 0 12 .5Z"/>
+    </svg>
+  `,
+  LinkedIn: `
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path fill="currentColor" d="M4.98 3.5A2.49 2.49 0 1 0 5 8.48a2.49 2.49 0 0 0-.02-4.98ZM3 9h4v12H3Zm7 0h3.83v1.64h.05c.53-1 1.84-2.05 3.8-2.05 4.06 0 4.82 2.54 4.82 5.84V21h-4v-5.72c0-1.36-.03-3.11-1.95-3.11-1.95 0-2.25 1.46-2.25 3V21h-4Z"/>
+    </svg>
+  `,
+  Facebook: `
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path fill="currentColor" d="M13.5 22v-8h2.7l.4-3h-3.1V9.1c0-.87.25-1.46 1.53-1.46H16.7V5.02c-.3-.04-1.3-.12-2.48-.12-2.45 0-4.12 1.45-4.12 4.13V11H7.3v3h2.8v8h3.4Z"/>
+    </svg>
+  `,
+  Instagram: `
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path fill="currentColor" d="M7.75 2h8.5A5.76 5.76 0 0 1 22 7.75v8.5A5.76 5.76 0 0 1 16.25 22h-8.5A5.76 5.76 0 0 1 2 16.25v-8.5A5.76 5.76 0 0 1 7.75 2Zm0 1.8A3.95 3.95 0 0 0 3.8 7.75v8.5A3.95 3.95 0 0 0 7.75 20.2h8.5a3.95 3.95 0 0 0 3.95-3.95v-8.5a3.95 3.95 0 0 0-3.95-3.95Zm8.95 1.35a1.1 1.1 0 1 1 0 2.2 1.1 1.1 0 0 1 0-2.2ZM12 6.86A5.14 5.14 0 1 1 6.86 12 5.14 5.14 0 0 1 12 6.86Zm0 1.8A3.34 3.34 0 1 0 15.34 12 3.34 3.34 0 0 0 12 8.66Z"/>
+    </svg>
+  `,
+  Email: `
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path fill="currentColor" d="M3 5h18a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Zm9 7.3L4 7.2V17h16V7.2Zm8-.74V7l-8 5-8-5v4.56l8 5 8-5Z"/>
+    </svg>
+  `
 };
 
 setText("nav-brand", portfolioContent.navBrand);
@@ -170,8 +200,9 @@ setHtml(
       (link) => `
         <a class="contact-link" href="${link.url}" ${
           link.url.startsWith("http") ? 'target="_blank" rel="noreferrer"' : ""
-        }>
-          ${link.label}
+        } aria-label="${link.label}" title="${link.label}">
+          <span class="contact-link-icon">${socialIcons[link.label] || ""}</span>
+          <span class="sr-only">${link.label}</span>
         </a>
       `
     )
